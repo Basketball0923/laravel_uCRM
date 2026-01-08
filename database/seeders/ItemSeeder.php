f43ca9e2aaa88415b6f71e790d94ce3c9fb6e881
Azusa <azusa@example.com>
2026-01-08 15:36:10 +0900

    itemsの下準備

---
+++ database/seeders/ItemSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * データベースに対するデータ設定の実行
+     */
+    public function run(): void
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
+use App\Http\Controllers\ItemController;
+
+// 顧客システムのルート
+Route::resource('items', ItemController::class)
+->middleware(['auth', 'verified']);
 
 Route::get('/inertia-test', function () {
         return Inertia::render('InertiaTest');
